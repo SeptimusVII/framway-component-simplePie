156c747590468c22becb93975e61c7f2fd252266
Quentin Vansteene <vansteene.quentin@gmail.com>
2024-05-15 18:02:32 +0200

    basic style, basic scripts

--- simplePie.js
+++ simplePie.js
@@ -1,3 +1,4 @@
+var Chart = require('chart.js/auto');
 module.exports = function(app){
     var SimplePie = Object.getPrototypeOf(app).SimplePie = new app.Component("simplePie");
     // SimplePie.debug = true;
@@ -8,8 +9,36 @@ module.exports = function(app){
     // SimplePie.loadingMsg     = "This message will display in the console when component will be loaded.";
     // SimplePie.requires       = [];
 
-    // SimplePie.prototype.onCreate = function(){
-    // do thing after element's creation
-    // }
+    SimplePie.prototype.onCreate = function(){
+        var pie = this;
+        var dataset;
+        try {
+            dataset = JSON.parse(pie.getAttr('dataset'))
+        } catch(e) {
+            if (window[pie.getAttr('dataset')] != undefined && typeof window[pie.getAttr('dataset')] == 'object') {
+               dataset = window[pie.getAttr('dataset')]; 
+            } else{
+                pie.$el.append('<p class="error">Can\'t load pie chart, invalid data provided</p>')
+                throw 'invalid data';
+            }
+        }
+        pie.canvas = pie.$el.append('<canvas></canvas>').find('canvas').get(0);
+        if (pie.getData('width',false)) pie.canvas.width = pie.getData('width',false);
+        
+
+        pie.chart = new Chart(pie.canvas,{
+            type: pie.getData('type','pie'),
+            data: {
+                labels: dataset.map(row => row[pie.getData('labels')]),
+                datasets: [
+                    {
+                        label: pie.getData('dataset-label'),
+                        data: dataset.map(row => row[pie.getData('dataset-data')]),
+                        backgroundColor: pie.getData('dataset-bgcolor',false) ? dataset.map(row => row[pie.getData('dataset-bgcolor')]) : false,
+                    },
+                ]
+            }
+        });
+    }
     return SimplePie;
 }
