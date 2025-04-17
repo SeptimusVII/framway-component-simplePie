79dc54b9b2e0055b5b68e8a772302c7c304dc6d9
SeptimusVII <vansteene.quentin@gmail.com>
2025-04-17 15:02:31 +0200

    update version

--- simplePie.js
+++ simplePie.js
@@ -3,8 +3,8 @@ module.exports = function(app){
     var SimplePie = Object.getPrototypeOf(app).SimplePie = new app.Component("simplePie");
     // SimplePie.debug = true;
     SimplePie.createdAt      = "2.5.0";
-    SimplePie.lastUpdate     = "2.5.0";
-    SimplePie.version        = "1";
+    SimplePie.lastUpdate     = "2.6.0";
+    SimplePie.version        = "1.1.0";
     // SimplePie.factoryExclude = true;
     // SimplePie.loadingMsg     = "This message will display in the console when component will be loaded.";
     // SimplePie.requires       = [];
